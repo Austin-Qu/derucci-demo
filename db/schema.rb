241c2446c8805edc0def7848f7e0ec697116be22
Anxing Qu <anxing.qu@gmail.com>
2017-08-29 10:32:14 +1000

    Added latest migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802062556) do
+ActiveRecord::Schema.define(version: 20170829003055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1025,6 +1025,7 @@ ActiveRecord::Schema.define(version: 20170802062556) do
     t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "engine", default: 0, null: false
     t.index ["active"], name: "index_spree_trackers_on_active"
   end
 
